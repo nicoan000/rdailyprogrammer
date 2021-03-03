dd8820b0d7111b271385d7b9e28eef8a793559a1
nicoan000 <nicoan000@gmail.com>
2021-03-02 22:21:38 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const cliProgress = require('cli-progress');
+const _colors = require('colors');
+import run from './scripts_by_day/2020/day12/part2';
+
+console.log('starting script');
+const b1 = new cliProgress.SingleBar({
+    format: 'CLI Progress |' + _colors.cyan('{bar}') + '| {percentage}% || {value}/{total} Chunks || Speed: {speed}\n\n',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
+});
+
+let res;
+let i = 0;
+let k = 0;
+let intervals = 1;
+let trials = 1;
+let need_bar = false;
+let time_lengths = [];
+
+if (need_bar) {
+    b1.start(trials * intervals, 0, {
+        speed: "N/A"
+    });
+}
+
+while (k !== trials) {
+    let initial_time = new Date();
+    while (i !== intervals) {
+        res = run();
+        i++;
+        b1.increment();
+    }
+    let final_time = new Date();
+    time_lengths.push(final_time - initial_time);
+    k++;
+    i = 0;
+}
+
+if (need_bar) {
+    b1.stop();
+}
+console.log('result ', res);
+console.log('average time: ', time_lengths.reduce((x, y) => x + y, 0) / time_lengths.length);
