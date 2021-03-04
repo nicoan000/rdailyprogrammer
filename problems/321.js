c161cf4dc328e17d69e9914f17f5c1638f025293
nicoan000 <nicoan000@gmail.com>
2021-03-04 02:17:37 -0800

    finished testing environment and fixed issue in problem 321

--- problems/321.js
+++ problems/321.js
@@ -1,47 +1,55 @@
 // https://www.reddit.com/r/dailyprogrammer/comments/6jr76h/20170627_challenge_321_easy_talking_clock/
 
+const test = (num) => {
+    const tens = {
+        "20": "twenty",
+        "30": "thirty",
+        "40": "fourty",
+        "50": "fifty"
+    };
+
+    if (tens[num]) {
+        return tens[num];
+    } else if (+num >= 21) {
+        return `${test(num.charAt(0) + 0)} ${test(0 + num.charAt(1))}`;
+    }
+
+    return {
+        "00": "twelve",
+        "01": "one",
+        "02": "two",
+        "03": "three",
+        "04": "four",
+        "05": "five",
+        "06": "six",
+        "07": "seven",
+        "08": "eight",
+        "09": "nine",
+        "10": "ten",
+        "11": "eleven",
+        "12": "twelve",
+        "13": "thirteen",
+        "14": "fourteen",
+        "15": "fifteen",
+        "16": "sixteen",
+        "17": "seventeen",
+        "18": "eighteen",
+        "19": "nineteen"
+    }[num];
+};
+
 export default (input) => {
     let pieces = ["It's"];
-    const timeNumArr = input.split(':');
-    // pieces.push(, [timeNumArr[0]])
+    let timeNumArr = input.split(':');
 
-    const test = (num) => {
-        const tens = {
-            "20": "twenty",
-            "30": "thirty",
-            "40": "fourty",
-            "50": "fifty"
-        };
-
-        if (tens[num]) {
-            return tens[num]
-        } else if (+num >= 21) {
-            return `${test(num.charAt(0) + 0)} ${test(0 + num.charAt(1))}`;
-        }
-
-        return {
-            "00": "twelve",
-            "01": "one",
-            "02": "two",
-            "03": "three",
-            "04": "four",
-            "05": "five",
-            "06": "six",
-            "07": "seven",
-            "08": "eight",
-            "09": "nine",
-            "10": "ten",
-            "11": "eleven",
-            "12": "twelve",
-            "13": "thirteen",
-            "14": "fourteen",
-            "15": "fifteen",
-            "16": "sixteen",
-            "17": "seventeen",
-            "18": "eighteen",
-            "19": "nineteen"
-        }[num];
-    };
+    let suffix;
+    if (+timeNumArr[0] <= 11) {
+        suffix = 'am';
+    } else {
+        suffix = 'pm';
+        const convertedNum = +timeNumArr[0] - 12;
+        timeNumArr[0] = `${convertedNum}`.length > 1 ? `${convertedNum}` : `0${convertedNum}`
+    }
 
     pieces = timeNumArr.reduce((y, x, i) => {
         if (i > 0 && x.charAt(0) == "0") {
@@ -57,11 +65,6 @@ export default (input) => {
         ]
     }, [...pieces]);
 
-    if (+timeNumArr[0] >= 12) {
-        pieces.push('pm');
-    } else {
-        pieces.push('am');
-    }
-
+    pieces.push(suffix);
     return pieces.join(' ') + '.';
 }
--- tests/321.test.js
+++ tests/321.test.js
@@ -1,5 +1,15 @@
 import solution from '../problems/321';
 
-test('outputs 12:14 in word form', () => {
-    expect(solution("12:14")).toBe("It's twelve fourteen");
+const cases = [
+    ["12:14", "It's twelve fourteen pm.", "outputs 12:14 in word form"],
+    ["00:59", "It's twelve fifty nine am.", "outputs 00:59 in word form"],
+    ["01:01", "It's one oh one am.", "outputs 1:01 in word form"],
+    ["05:23", "It's five twenty three am.", ""],
+    ["14:34", "It's two thirty four pm.", ""],
+    ["18:45", "It's six fourty five pm.", ""],
+    ["23:55", "It's eleven fifty five pm.", ""],
+];
+
+test.each(cases)('test', (a, b, expected) => {
+    expect(solution(a)).toBe(b);
 });
